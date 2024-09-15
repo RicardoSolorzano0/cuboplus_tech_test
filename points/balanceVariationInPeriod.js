4b6513137a126dea013d7404aac949f0aae3e0b1
Ricardo Solorzano <ricardosolorzano97@gmail.com>
2024-09-15 15:24:34 -0600

    refactorizando funcion

--- points/balanceVariationInPeriod.js
+++ points/balanceVariationInPeriod.js
@@ -1,24 +1,29 @@
-const { mempoolApi } = require("../api/apiMempool")
+const { getUnixTime } = require("date-fns");
+const { mempoolApi } = require("../api/apiMempool");
+const { subDays } = require("date-fns/subDays");
 
 const getBalanceVariation = async (address, days) => {
     try {
         const response = await mempoolApi.get(`${address}/txs`);
         const transactions = response.data;
-        const currentTime = Math.floor(Date.now() / 1000);
-        const periodStart = currentTime - (days * 24 * 60 * 60);
+        const currentDate = new Date();
+
+        const daysagoDate = subDays(currentDate, days);
+        const daysagoTime = getUnixTime(daysagoDate);
+
         let balanceVariation = 0;
 
-        transactions.forEach(tx => {
-            if (tx.status.confirmed && tx.status.block_time >= periodStart) {
-                tx.vout.forEach(vout => {
+        transactions.map(tx => {
+            if (tx.status.confirmed && tx.status.block_time >= daysagoTime) {
+                tx.vout.map(vout => {
                     if (vout.scriptpubkey_address === address) {
                         balanceVariation += parseInt(vout.value);
                     }
                 });
             }
-        });
+        })
 
-        return balanceVariation;
+        return balanceVariation
     } catch (error) {
         console.error("Error in fetching data: ", error);
     }
@@ -26,5 +31,5 @@ const getBalanceVariation = async (address, days) => {
 
 
 module.exports = {
-    getBalanceVariation
+    getBalanceVariation,
 }
